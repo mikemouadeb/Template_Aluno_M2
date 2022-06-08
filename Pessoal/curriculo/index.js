3d6f349290cf64104095d331297228b6e049c0df
mikemouadeb <mike.mouadeb@sou.inteli.edu.br>
2022-06-08 16:02:17 -0300

    curriculo novo

--- Pessoal/curriculo/index.js
+++ Pessoal/curriculo/index.js
@@ -2,8 +2,8 @@ function pegarDados() {
     fetch('http://localhost:3000/')
         .then(response => response.json())
         .then(data => data.forEach(element => {
-            const nome = `<p> ${element.nome} </p>`
-            const idade = `<p> ${element.idade} </p>`
+            const nome = `<p>Nome: ${element.nome} </p>`
+            const idade = `<p>Idade: ${element.idade} </p>`
             const contato = `<p> ${element.contato} </p>`
             const email = `<p> ${element.email} </p>`
             const endereco = `<p> ${element.endereco} </p>`
