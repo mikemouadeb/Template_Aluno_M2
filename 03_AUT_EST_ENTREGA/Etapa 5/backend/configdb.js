b1cc55a3ffed2c76003c6dc397bd298ef8d55e08
mikemouadeb <mike.mouadeb@sou.inteli.edu.br>
2022-06-13 20:14:04 -0300

    mudanças.10

--- 03_AUT_EST_ENTREGA/Etapa 5/backend/configdb.js
+++ 03_AUT_EST_ENTREGA/Etapa 5/backend/configdb.js
@@ -1,9 +1,13 @@
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 
-export async function openDb() {
-    return open({
+export async function openDb() 
+{
+    return open
+    ({
         filename: './database.db',
         driver: sqlite3.Database
     })
 }
+
+//conectando com o banco de dados 
